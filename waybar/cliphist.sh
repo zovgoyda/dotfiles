7ad4781bfec1165187c23e42b23b039b256f8537
zovgoyda <comradegreen@yandex.ru>
2026-07-05 23:56:37 +0400

    Update cliphist.sh

--- waybar/cliphist.sh
+++ waybar/cliphist.sh
@@ -1,6 +1,37 @@
 #!/usr/bin/env bash
-if [ -z "$@" ]; then
-    cliphist list
+
+# 1. Подгружаем цвета Pywal
+WAL_COLORS="$HOME/.cache/wal/colors.sh"
+if [ -f "$WAL_COLORS" ]; then
+    source "$WAL_COLORS"
 else
+    background="#2e1a47"
+    color4="#4a2c73"
+    color5="#4a2c73"
+fi
+
+# 2. Создаем временный файл стилей для wofi
+WOFI_STYLE=$(mktemp)
+cat <<EOF > "$WOFI_STYLE"
+window {
+    background-color: $background;
+}
+#entry:selected {
+    background-color: $color4;
+}
+#text:selected {
+    color: $color5;
+}
+EOF
+
+# 3. Логика cliphist + wofi с применением стиля
+if [ -z "$@" ]; then
+    # Показываем меню wofi со стилями, выбранный элемент передается этому же скрипту
+    cliphist list | wofi --dmenu --style "$WOFI_STYLE" | xargs -I {} "$0" "{}"
+else
+    # Декодируем и копируем в буфер обмена
     echo "$@" | cliphist decode | wl-copy
 fi
+
+# 4. Удаляем временный файл стиля
+rm "$WOFI_STYLE"
